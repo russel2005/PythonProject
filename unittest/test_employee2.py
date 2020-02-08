f03fc27e0ed2ac3797405f3e678a5f9c22fc9972
russel2005 <russel2005@gmail.com>
2020-02-08 16:38:21 -0500

    update test_employee2.py

--- unittest/test_employee2.py
+++ unittest/test_employee2.py
@@ -4,22 +4,33 @@ from employee import Employee
 
 class TestEmployee(unittest.TestCase):
 
+    @classmethod
+    def setUpClass(cls):
+        print('setupClass')
+
+    @classmethod
+    def tearDownClass(cls):
+        print('teardownClass')
     def setUp(self):
+        print('setUp')
         self.emp_1 = Employee('russel', 'islam', 90000)
         self.emp_2 = Employee('arish', 'raheel', 50000)
 
     def tearDown(self):
-        pass
+        print('tearDown\n')
 
     def test_email(self):
+        print('test_email')
         self.assertEqual(self.emp_1.email, 'russel.islam@email.com')
         self.assertEqual(self.emp_2.email, 'arish.raheel@email.com')
 
     def test_fullname(self):
+        print('test_fullname')
         self.assertEqual(self.emp_1.fullname, 'russel islam')
         self.assertEqual(self.emp_2.fullname, 'arish raheel')
 
     def test_apply_raise(self):
+        print('test_apply_raise')
         self.emp_1.apply_raise()
         self.emp_2.apply_raise()
 
