0a9bb04fde2f683af720a4993037e5bb2c9fe7f0
Touhidul Islam <russel2005@gmail.com>
2020-03-10 14:15:51 -0400

    Update Day11_file.py

--- practice/Day11_file.py
+++ practice/Day11_file.py
@@ -6,13 +6,31 @@ r+ > read and write
 a > append
 """
 
+###Write a file
 my_list = [1,2,3]
 
 my_file = open("firstfile.txt", "w")
 
 for item in my_list:
     my_file.write(str(item))
+    #my_file.write(str(item) + "\n")
     
 my_file.close()
 
+###Read a file
+"""
+Feading a file > .read()
+reading line by line > .readline()
+"""
+my_file = open("firstfile.txt", "r")
+print(str(my_file.read())) # whole file
+my_file.close()
+
+print("Line by line =============>")
+my_file = open("firstfile.txt", "r")
+print(str(my_file.readline())) #read firstline only
+my_file.close()
+
+
+############
 
