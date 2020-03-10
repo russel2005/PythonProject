8db23787f4a7070a6ad481a2d353176aa47f1c43
Touhidul Islam <russel2005@gmail.com>
2020-03-10 16:12:57 -0400

    Update Day12-Class.py

--- practice/Day12-Class.py
+++ practice/Day12-Class.py
@@ -5,7 +5,9 @@ class Employee:
     self.last = last
     self.pay = pay
     self.email = first + '.' + last + '@company.com'
-    
+  
+  def fullname(self):
+      return '{} {}'.format(self.first, self.last)
     
 emp_1 = Employee('russel', 'islam', 4000)
 emp_2 = Employee('arish', 'islam', 5000)
@@ -13,3 +15,5 @@ emp_2 = Employee('arish', 'islam', 5000)
 print(emp_1.email)
 print(emp_2.email)
 
+print('{} {}'.format(emp_1.first, emp_1.last))
+print(emp_1.fullname)
