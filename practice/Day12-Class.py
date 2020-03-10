e4938f60c19a9507692095ebd789f122e31a51ab
Touhidul Islam <russel2005@gmail.com>
2020-03-10 16:10:11 -0400

    Update and rename Class.py to Day12-Class.py

--- practice/Day12-Class.py
+++ practice/Day12-Class.py
@@ -5,3 +5,11 @@ class Employee:
     self.last = last
     self.pay = pay
     self.email = first + '.' + last + '@company.com'
+    
+    
+emp_1 = Employee('russel', 'islam', 4000)
+emp_2 = Employee('arish', 'islam', 5000)
+
+print(emp_1.email)
+print(emp_2.email)
+
