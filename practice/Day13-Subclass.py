574e7530e669c5c7e9fb354d51efd3835003f94d
Touhidul Islam <russel2005@gmail.com>
2020-03-10 17:40:12 -0400

    Update Day13-Subclass.py

--- practice/Day13-Subclass.py
+++ practice/Day13-Subclass.py
@@ -12,13 +12,54 @@ class Employee:
       
     def apply_raise(self):
        self.pay = int(self.pay * self.raise_amount) 
-       
+
+### sub class ######
 class Developer(Employee):
-  pass
+    raise_amt = 1.10
+
+    def __init__(self, first, last, pay, prog_lang):
+        super().__init__(first, last, pay)
+        self.prog_lang = prog_lang
 
 
-dev_1 = Developer('russel', 'islam', 4000)
-dev_2 = Developer('arish', 'islam', 5000)
+dev_1 = Developer('russel', 'islam', 4000, 'python')
+dev_2 = Developer('arish', 'islam', 5000, 'java')
 
 print(dev_1.email)
-print(dev_2.email)
+print(dev_2.prog_lang)
+
+### sub class #####
+
+class Manager (Employee):
+    
+    def __init__(self, first, last, pay, employees=None):
+        super().__init__(first, last, pay)
+        if employees is None:
+            self.employees = []
+        else:
+            self.employees = employees
+        
+    def add_emp(self, emp):
+        if emp not in self.employees:
+            self.employees.append(emp)
+    
+    def remove_emp(self, emp):
+        if emp in self.employees:
+            self.employees.remove(emp)
+            
+    def print_emps(self):
+        for emp in self.employees:
+            print('-->', emp.fullname())
+    
+mgr_1 = Manager('Sue', 'smith', 90000, [dev_1])
+print(mgr_1.email)
+mgr_1.add_emp(dev_2)
+mgr_1.print_emps()
+
+#######  build in  isinstance() method, issubclass()
+
+print(isinstance(mgr_1, Manager) # True
+print(isinstance(mgr_1, Developer) # False
+      
+print(issubclass(Developer, Employee) # True
+print(issubclass(Manager, Developer) # False
