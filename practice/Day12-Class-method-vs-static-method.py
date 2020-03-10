2a87fdfd83bc8504941fdc33bff178912a34ef03
Touhidul Islam <russel2005@gmail.com>
2020-03-10 16:48:04 -0400

    Create Day12-Class-method-vs-static-method.py

---
+++ practice/Day12-Class-method-vs-static-method.py
@@ -0,0 +1,32 @@
+class Employee:
+    raise_amount = 1.04 #instance variable
+    num_of_emps = 0 #class variable, if we want to increase empy by instance then we should use class name instead of self.
+
+    def __init__(self, first, last, pay):
+        self.first = first
+        self.last = last
+        self.pay = pay
+        self.email = first + '.' + last + '@company.com'
+        Employee.num_of_emps += 1  #we should not use self because we want to increase class level.
+  
+    def fullname(self):
+        return '{} {}'.format(self.first, self.last)
+      
+    def apply_raise(self):
+       self.pay = int(self.pay * self.raise_amount) #use instance variable with self.
+       #self.pay = int(self.pay * Employee.raise_amount) 
+       #above line will raise pay by all employee
+       
+    @classmethod
+    def set_raise_amt(cls, amount):
+        cls.raise_amt = amount
+   
+emp_1 = Employee('russel', 'islam', 4000)
+emp_2 = Employee('arish', 'islam', 5000)
+
+Employee.set_raise_amt(1.05)
+
+print(Employee.raise_amt)
+print(emp_1.raise_amt)
+print(emp_2.raise_amt)
+
