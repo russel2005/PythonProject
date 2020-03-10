9e0b6a3d6ebe1bbc5f127ec4ccef617d3fb5cc7b
Touhidul Islam <russel2005@gmail.com>
2020-03-10 17:13:05 -0400

    Update Day13-Subclass.py

--- practice/Day13-Subclass.py
+++ practice/Day13-Subclass.py
@@ -1,1 +1,24 @@
+class Employee:
+    raise_amount = 1.04 
+   
+    def __init__(self, first, last, pay):
+        self.first = first
+        self.last = last
+        self.pay = pay
+        self.email = first + '.' + last + '@company.com'
+          
+    def fullname(self):
+        return '{} {}'.format(self.first, self.last)
+      
+    def apply_raise(self):
+       self.pay = int(self.pay * self.raise_amount) 
+       
+class Developer(Employee):
+  pass
 
+
+dev_1 = Developer('russel', 'islam', 4000)
+dev_2 = Developer('arish', 'islam', 5000)
+
+print(dev_1.email)
+print(dev_2.email)
