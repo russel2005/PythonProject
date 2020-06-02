9613de5340b04bbf61cd82a360b4d3885124ca4d
Touhidul islam <russel2005@gmail.com>
2020-06-02 14:17:57 -0400

    code challenges added

---
+++ practice/codechallenges/FindPrimeFactors.py
@@ -0,0 +1,12 @@
+def get_prime_factors(N):
+    factors = list()
+    divisor = 2
+    while(divisor <= N):
+        if (N % divisor) == 0:
+            factors.append(divisor)
+            N = N/divisor
+        else:
+            divisor += 1
+    return factors
+
+print(get_prime_factors(630))
