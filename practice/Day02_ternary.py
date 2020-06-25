e975ae7cca61c77e707c2e5858b9f28c0e77b23c
russel2005 <russel2005@gmail.com>
2020-06-24 21:52:31 -0400

    updated Day02_ternary.py

--- practice/Day02_ternary.py
+++ practice/Day02_ternary.py
@@ -4,6 +4,6 @@ if age >= 18:
 else:
     message = "Not eligible"
 
-
+# its also called conditional expression, or same as ternary like java
 message = "Eligible" if age >= 18 else "Not eligible"
 print(message)
