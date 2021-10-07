27b8fc935bbe33c6d25c7a8ddc33e70fffc8c31d
Touhidul Islam <russel2005@gmail.com>
2021-10-06 23:01:29 -0400

    Update fist-class_function.py

--- advance/fist-class_function.py
+++ advance/fist-class_function.py
@@ -1,1 +1,12 @@
 
+def square(x):
+  return x*x
+
+def my_map(func, arg_list):
+  result = []
+  for i in arg_list:
+    result.append(func(i))
+  return result
+
+squares = my_map(square, [1,2,3,4,5])
+print(squares)
