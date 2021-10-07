45e3f2e71a3b7d89855599978418b8820345cc2d
Touhidul Islam <russel2005@gmail.com>
2021-10-06 22:32:36 -0400

    Update Decorators.py

--- advance/Decorators.py
+++ advance/Decorators.py
@@ -1,6 +1,12 @@
+#first class fuction
+def square(x):
+  return x*x
+
+f = square
+print(square)//output: <function square at 0x101978950>
+print(f(5))//output: 25
+
 #Decorators
-
-
 def outer_function():
   msg = 'Hi'
   
